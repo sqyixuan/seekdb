1b340783f7d488bc9987347bb0de7fded5ea9516
Sync Agent <sync@oceanbase.com>
2026-03-27 22:35:19 +0800

    Code sync: task/2025110400111790752

--- rpm/seekdb-build.sh
+++ rpm/seekdb-build.sh
@@ -8,15 +8,6 @@ RELEASE=$4
 CURDIR=$PWD
 TOP_DIR=`pwd`/../
 
-# check os type
-PACKAGE_TYPE="rpm"
-PACKAGE_EXTENSION="rpm"
-OS_TYPE="$(uname -s)" || exit 1
-if [[ "${OS_TYPE}" == "Darwin" ]]; then
-  PACKAGE_TYPE="tgz"
-  PACKAGE_EXTENSION="tar.gz"
-fi
-
 OB_DISABLE_LSE_OPTION=""
 [[ $OB_DISABLE_LSE == "1" ]] && OB_DISABLE_LSE_OPTION="-DOB_DISABLE_LSE=ON"
 
@@ -25,14 +16,14 @@ echo "[BUILD] args: TOP_DIR=${TOP_DIR} PROJECT_NAME=${PROJECT_NAME} VERSION=${VE
 cd ${TOP_DIR}
 ./build.sh clean
 ./build.sh                    \
-    ${PACKAGE_TYPE}           \
+    rpm                       \
     -DOB_RELEASEID=$RELEASE   \
     -DBUILD_NUMBER=$RELEASE   \
     -DUSE_LTO_CACHE=ON	      \
     ${OB_DISABLE_LSE_OPTION}  \
     --init                    \
-    --make ${PACKAGE_TYPE} || exit 1
+    --make rpm || exit 1
 
-cd ${TOP_DIR}/build_${PACKAGE_TYPE}
-mv *.${PACKAGE_EXTENSION} $CURDIR || exit 2
+cd ${TOP_DIR}/build_rpm
+mv *.rpm $CURDIR || exit 2
 
