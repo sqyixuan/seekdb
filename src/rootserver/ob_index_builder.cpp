a9b1c755f8f9fbbfc6963fe9e04bce25f5496492
Contributors <41898282+github-actions[bot]@users.noreply.github.com>
2026-03-28 01:44:35 +0800

    [CP] [fix] vector index offline ddl hung when create failed

--- src/rootserver/ob_index_builder.cpp
+++ src/rootserver/ob_index_builder.cpp
@@ -685,8 +685,7 @@ int ObIndexBuilder::submit_build_index_task(
   param.tenant_data_version_ = tenant_data_version;
   param.new_snapshot_version_ = new_fetched_snapshot;
   const bool is_fts_or_multivalue = share::schema::is_fts_or_multivalue_index(create_index_arg.index_type_);
-  const bool is_vec_rowkey_vid_aux = share::schema::is_vec_rowkey_vid_type(create_index_arg.index_type_);
-  param.ddl_need_retry_at_executor_ = (is_fts_or_multivalue || is_vec_rowkey_vid_aux) && !create_index_arg.is_offline_rebuild_;
+  param.ddl_need_retry_at_executor_ = is_fts_or_multivalue && !create_index_arg.is_offline_rebuild_;
   if (OB_UNLIKELY(nullptr == data_schema || nullptr == index_schema || tenant_data_version <= 0)) {
     ret = OB_INVALID_ARGUMENT;
     LOG_WARN("schema is invalid", K(ret), KP(data_schema), KP(index_schema), K(tenant_data_version));
@@ -1485,11 +1484,6 @@ int ObIndexBuilder::do_create_local_index(
                      global_index_without_column_info,
                      true /*generate_id*/, index_schema))) {
         LOG_WARN("fail to generate schema", K(ret), K(my_arg));
-      }
-      // create empty major for domain table
-      if (OB_FAIL(ret)) {
-      } else if (index_schema.is_vec_delta_buffer_type() || index_schema.is_hybrid_vec_index_log_type()) {
-        index_schema.set_index_status(INDEX_STATUS_AVAILABLE);
       }
       if (OB_FAIL(ret)) {
       } else if (OB_FAIL(new_table_schema.check_create_index_on_hidden_primary_key(index_schema))) {
